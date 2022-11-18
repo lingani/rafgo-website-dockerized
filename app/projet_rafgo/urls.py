1056b0b0da3e566a87ee304b14c391d9472c9dac
lingani <malick.lingani@gmail.com>
2022-11-18 11:11:41 +0000

    added login and account urls

--- app/projet_rafgo/urls.py
+++ app/projet_rafgo/urls.py
@@ -21,15 +21,60 @@ from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Core Django imports for auth
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns=i18n_patterns(
     path('', include('website.urls')),
     path(_('admin/'), admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('blog/', include('blog.urls')),
-    path('api/v1/', include('blog.api.v1.routers.routers')) 
+    path('blog/', include('blog.urls', namespace='blog')), # Urls for blog app.
+    path('api/v1/', include('blog.api.v1.routers.routers')), 
     # re_path(r'^rosetta/', include('rosetta.urls'))
+
+    ########## - urls for accounts 
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('api/v1/article/', include('blog.api.v1.routers.routers')), # Urls for API.
+
+    # Url for password reset.
+    path('account/password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='account/password_reset.html'),
+         name='password_reset'),
+
+    # Url for successful password reset.
+    path('account/password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='account/password_reset_done.html'),
+         name='password_reset_done'),
+
+    # Url for successful password reset confirm.
+    path('account/password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='account/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    # Url for password reset done.
+    path('account/password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='account/password_reset_complete.html'),
+         name='password_reset_complete'),
+
+    ##########
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+
+    import debug_toolbar
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
+
+
+# Modifies default django admin titles and headers with custom app detail.
+admin.site.site_header = "RAFGO Admin"
+admin.site.site_title = "RAFGO Admin Portal"
+admin.site.index_title = "Welcome to RAFGO Platform Portal"
